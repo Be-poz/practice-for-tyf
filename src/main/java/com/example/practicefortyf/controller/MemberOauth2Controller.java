b6bd7968f2816fea6b627e41b6d210e8e92c8191
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-07-05 01:43:46 +0900

    feat: 네이버 로그인 추가

--- src/main/java/com/example/practicefortyf/controller/MemberOauth2Controller.java
+++ src/main/java/com/example/practicefortyf/controller/MemberOauth2Controller.java
@@ -45,7 +45,7 @@ public class MemberOauth2Controller {
         HttpEntity<Object> userInfoRequest = new HttpEntity<>(headers);
         ResponseEntity<String> userInfoResponse = rt.exchange(
                 "https://kapi.kakao.com//v2/user/me",
-                HttpMethod.GET,
+                HttpMethod.POST,
                 userInfoRequest,
                 String.class
         );
@@ -56,4 +56,51 @@ public class MemberOauth2Controller {
 
         return nickname;
     }
+
+    @GetMapping("/naver")
+    public Object naverOauthRedirect(@RequestParam String code, @RequestParam String state) {
+        RestTemplate rt = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        LinkedMultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "3QbJSuUX3a7bIi9RDukT");
+        params.add("client_secret", "kIJadJ3_IQ");
+        params.add("code", code);
+        params.add("state", state);
+
+        HttpEntity<LinkedMultiValueMap<String, String>> tokenRequest = new HttpEntity<>(params, headers);
+
+        ResponseEntity<String> tokenResponse = rt.exchange(
+                "https://nid.naver.com/oauth2.0/token",
+                HttpMethod.POST,
+                tokenRequest,
+                String.class
+        );
+
+        JSONObject tokenJsonObject = new JSONObject(tokenResponse.getBody());
+        String accessToken = tokenJsonObject.getString("access_token");
+        String refreshToken = tokenJsonObject.getString("refresh_token");
+        String tokenType = tokenJsonObject.getString("token_type");
+
+        headers.add("Authorization", tokenType + " " + accessToken);
+        HttpEntity<Object> userInfoRequest = new HttpEntity<>(headers);
+
+        ResponseEntity<String> userInfoResponse = rt.exchange(
+                "https://openapi.naver.com/v1/nid/me",
+                HttpMethod.GET,
+                userInfoRequest,
+                String.class
+        );
+
+        JSONObject rawUserInfoJsonObject = new JSONObject(userInfoResponse.getBody());
+        JSONObject userInfoJsonObject = (JSONObject) rawUserInfoJsonObject.get("response");
+        String birthday = userInfoJsonObject.getString("birthday");
+        String name = userInfoJsonObject.getString("name");
+        String email = userInfoJsonObject.getString("email");
+
+        return "birthday: " + birthday + " name: " + name + " email : " + email;
+    }
+
 }
